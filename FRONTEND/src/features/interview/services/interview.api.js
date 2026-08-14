15b00524e710cf79e4dfa12865df970e0a86d510
Om Aggarwal <omaggarwal.work@gmail.com>
2026-08-14 18:57:40 +0530

    Configure frontend API URLs

--- FRONTEND/src/features/interview/services/interview.api.js
+++ FRONTEND/src/features/interview/services/interview.api.js
@@ -1,9 +1,5 @@
 import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:3000",
-  withCredentials: true,
-});
+import api from "../../../config/axios";
 
 /**
  * @description Service to generate interview report based on your self description, job description and resume
